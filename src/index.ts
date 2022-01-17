63e8b29f64233b2f86906de22dbb60848d434831
Andreas Forster <acfo@gmx.net>
2022-01-17 17:51:18 +0100

    chore(projen): implement private bucket

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,16 @@
-export class Hello {
-  public sayHello() {
-    return "hello, world!";
+import * as s3 from "@aws-cdk/aws-s3";
+
+import { Construct } from "@aws-cdk/core";
+
+export interface PrivateBucketProps extends s3.BucketProps {}
+
+export class PrivateBucket extends s3.Bucket {
+  constructor(scope: Construct, id: string, props: PrivateBucketProps = {}) {
+    super(scope, id, {
+      encryption: s3.BucketEncryption.S3_MANAGED,
+      ...props,
+      blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
+      enforceSSL: true,
+    });
   }
 }
