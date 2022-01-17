f30d3df5781b72ee1bfe4d259022903daf0b4b65
Andreas Forster <acfo@gmx.net>
2022-01-17 16:10:51 +0100

    chore(projen): add prettier

--- .projenrc.js
+++ .projenrc.js
@@ -1,12 +1,18 @@
-const { awscdk } = require('projen');
+const { awscdk } = require("projen");
 const project = new awscdk.AwsCdkConstructLibrary({
-  author: 'Andreas Forster',
-  authorAddress: 'acfo@gmx.net',
-  cdkVersion: '2.1.0',
-  defaultReleaseBranch: 'main',
-  name: 'cdk-private-bucket',
-  repositoryUrl: 'https://github.com/pepperize/cdk-private-bucket.git',
-  gitignore: ['.idea/', '*.iml'],
+  author: "Andreas Forster",
+  authorAddress: "acfo@gmx.net",
+  cdkVersion: "2.1.0",
+  defaultReleaseBranch: "main",
+  name: "cdk-private-bucket",
+  repositoryUrl: "https://github.com/pepperize/cdk-private-bucket.git",
+  prettier: true,
+  prettierOptions: {
+    settings: {
+      printWidth: 120,
+    },
+  },
+  gitignore: [".idea/", "*.iml"],
 
   // deps: [],                /* Runtime dependencies of this module. */
   // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
