122831be7f8201dcdf48b5d6c455dfa94dfafa2a
Patrick Florek <patrick.florek@gmail.com>
2022-10-12 09:45:22 +0200

    chore: barrel export construct

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,1 @@
-import { aws_s3 } from "aws-cdk-lib";
-import { Construct } from "constructs";
-
-export interface PrivateBucketProps extends aws_s3.BucketProps {}
-
-export class PrivateBucket extends aws_s3.Bucket {
-  constructor(scope: Construct, id: string, props: PrivateBucketProps = {}) {
-    super(scope, id, {
-      encryption: aws_s3.BucketEncryption.S3_MANAGED,
-      ...props,
-      blockPublicAccess: aws_s3.BlockPublicAccess.BLOCK_ALL,
-      enforceSSL: true,
-    });
-  }
-}
+export * from "./private-bucket";
